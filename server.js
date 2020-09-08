2566e90143fa20be90dccdf225c12b0a69e10207
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-08 14:32:48 -0400

    setup test route in user route file

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API Working');
-});
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
