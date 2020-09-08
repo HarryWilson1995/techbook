18c83f0b0c45313d3fb5bb5656e5ff539970702c
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-08 16:41:27 -0400

    check if user exists by email before registering

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -15,12 +16,23 @@ router.post(
       'Please enter a password with 7 or more characters'
     ).isLength({ min: 7 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
