0eecf7947cfbc9da3ea539376bb7727cbbf8a625
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-09 17:32:27 -0400

    build out logic for create a new post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,7 +5,7 @@ const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
 const User = require('../../models/User');
-const checkObjectId = require('../../middleware/checkObjectId');
+const Profile = require('../../models/Profile');
 
 // @route    POST api/posts
 // @desc     Create a post
@@ -17,6 +17,24 @@ router.post(
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
     }
   }
 );
