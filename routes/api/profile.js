1355626423f4fbeab37a750ae96e84e2bb5a156f
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-09 13:54:38 -0400

    create new experience, find the profile and add the experience to that profiles experience array

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -171,7 +171,45 @@ router.put(
         .custom((value, { req }) => (req.body.to ? value < req.body.to : true)),
     ],
   ],
-  async (req, res) => {}
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
 );
 
 module.exports = router;
