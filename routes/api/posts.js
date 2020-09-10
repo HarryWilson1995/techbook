d982bb2038d7e64e9bffc6e230d647bfcaa3795c
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-09 20:44:44 -0400

    build out delete comment route with checks for comment existing and user owning comment, all basic routes now setup and tested thoroughly with postman

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -180,4 +180,36 @@ router.post(
   }
 );
 
+// @route    DELETE api/posts/comment/:id/:comment_id
+// @desc     Delete comment
+// @access   Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' });
+    }
+
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
