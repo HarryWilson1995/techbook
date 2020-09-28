0024c66e084acd0ea88df713a8ec6cd51873784a
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-28 18:43:24 -0400

    setup clear profile action which clears out profile state when user logs out

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,6 +8,7 @@ import {
   LOGIN_FAIL,
   LOGIN_SUCCESS,
   LOGOUT,
+  CLEAR_PROFILE,
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
@@ -95,6 +96,9 @@ export const login = (email, password) => async (dispatch) => {
 
 // Logout
 export const logout = () => (dispatch) => {
+  dispatch({
+    type: CLEAR_PROFILE,
+  });
   dispatch({
     type: LOGOUT,
   });
