3907e175bcf8d6a8b42a331d1c73da374887b5ad
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-29 18:37:08 -0400

    build out postItem component which will display post, likes, comments

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-// import PostItem from './PostItem';
+import PostItem from './PostItem';
 // import PostForm from './PostForm';
 import { getPosts } from '../../actions/post';
 
@@ -11,18 +11,20 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     getPosts();
   }, [getPosts]);
 
-  return (
+  return loading ? (
+    <Spinner />
+  ) : (
     <Fragment>
       <h1 className='large text-primary'>Posts</h1>
       <p className='lead'>
         <i className='fas fa-user' /> Welcome to the community
       </p>
-      {/* <PostForm />
-      <div className="posts">
+      {/* <PostForm /> */}
+      <div className='posts'>
         {posts.map((post) => (
           <PostItem key={post._id} post={post} />
         ))}
-      </div> */}
+      </div>
     </Fragment>
   );
 };
