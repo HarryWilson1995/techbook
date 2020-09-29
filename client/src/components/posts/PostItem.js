c9be580dea46b6bd71f47277f76799cf1625ac71
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-29 18:46:25 -0400

    create update_likes type, actions for like and dislike and reducer case

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -50,7 +50,7 @@ const PostItem = ({
               type='button'
               className='btn btn-danger'
             >
-              <i className='fas fa-times' />
+              <i className='fas fa-trash-alt' />
             </button>
           )}
         </Fragment>
