f20c5f02fdb979cff0b63ea1ebf0a656b68bae76
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-29 11:42:02 -0400

    build out profileItem component and mount within Profiles

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
+import ProfileItem from './ProfileItem';
 import { connect } from 'react-redux';
 import { getProfiles } from '../../actions/profile';
 
@@ -8,7 +9,30 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
     getProfiles();
   }, []);
-  return <div></div>;
+  return (
+    <Fragment>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop p-r-half'></i> Browse and
+            Connect with Developers
+          </p>
+          <div className='profiles'>
+            {profiles.length > 0 ? (
+              profiles.map((profile) => (
+                <ProfileItem key={profile._id} profile={profile} />
+              ))
+            ) : (
+              <h4>No profiles found...</h4>
+            )}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profiles.propTypes = {
