74716540124b1ba5d8ac11b3ad1eccd374c63c04
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-29 10:51:47 -0400

    add delete account functionality to dashboard and update backend route to also delete users posts when account deleted

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -147,7 +148,7 @@ router.get('/user/:user_id', async (req, res) => {
 // @access  Private
 router.delete('/', auth, async (req, res) => {
   try {
-    // Remove users posts later!
+    await Post.deleteMany({ user: req.user.id });
     await Profile.findOneAndRemove({ user: req.user.id });
     await User.findOneAndRemove({ _id: req.user.id });
     res.json({ msg: 'User Account Deleted' });
