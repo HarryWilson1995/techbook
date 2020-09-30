043e6fb31546ee91378e5a09bc55eb63857c1e79
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-30 06:53:33 -0400

    create deleteComment action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -144,3 +144,21 @@ export const addComment = (postId, formData) => async (dispatch) => {
     });
   }
 };
+
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
